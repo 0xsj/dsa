86cd4936bfec98d36f2ff2bce1ab6b8bb70b510d
sjtommylee <sjtommylee@gmail.com>
2023-05-15 20:01:55 -0700

    try 1

--- arrays-hashing/contains-duplicate/cd.py
+++ arrays-hashing/contains-duplicate/cd.py
@@ -4,6 +4,16 @@ from typing import List
 a = [1, 2, 3, 4] # false
 b = [1, 2, 3, 1] # true
 
+
+"""
+# Try 1
+1. instantiate a class named "Solution"
+2. method named containsDuplicate, which takes in a list of integers. expect a boolean return
+3. first loop, we just go for the entire length of nums
+4. the second for loop, we start at indices i + 1 to length of nums
+5. we check to see if nums[i] == nums[j]. because if there are two identical values in different positions, there is a "duplicate"
+6. return True if there is a match, otherwise return False 
+"""
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         for i in range(len(nums)):
