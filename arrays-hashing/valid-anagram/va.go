74ea4102d2fa3a46acd3df0724137a2fdcfe07af
sjtommylee <sjtommylee@gmail.com>
2023-05-15 21:49:41 -0700

    chore: commit save point

--- arrays-hashing/valid-anagram/va.go
+++ arrays-hashing/valid-anagram/va.go
@@ -35,6 +35,34 @@ func isAnagram(s string, t string) bool {
 	return false
 }
 
+// Try 2 - corrected
+// 1. in try 1, the sums of the rune values would be an issue.
+// 2. we first compare the length of s to t
+// 3. we initialize a map "count" to store the frequency of each character in the strings
+// 4. iterate through s, increment count for each character in s
+// 5. iterate through , but we decrement for each character. if a the count becomes negative or if a characer in t does not exist in the map, we return false
+
+func isAnagram2(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	count := make(map[rune]int)
+
+	for _, r := range s {
+		count[r]++
+	}
+
+	for _, r := range t {
+		count[r]--
+		if count[r] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	fmt.Println(isAnagram(s, t))
 }
