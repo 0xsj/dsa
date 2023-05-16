c305b71da119f95efa0ea6ab552443e6fb5c4fe2
sjtommylee <sjtommylee@gmail.com>
2023-05-15 21:59:50 -0700

    chore: commit save point

--- arrays-hashing/contains-duplicate/cd.go
+++ arrays-hashing/contains-duplicate/cd.go
@@ -1,1 +1,22 @@
 package main
+
+var a []int
+
+func containsDuplicate(nums []int) bool {
+	seen := make(map[int]bool)
+
+	for _, num := range nums {
+		if seen[num] {
+			return true
+		}
+		seen[num] = true
+	}
+	return false
+}
+
+func main() {
+	// a := []int{1, 2, 3, 4, 5}
+	b := []int{1, 1, 2, 3, 4}
+	result := containsDuplicate(b)
+	println(result)
+}
