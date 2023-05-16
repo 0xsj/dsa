adea15e3dacb8be58b36e109ac33dc43fae6729a
sjtommylee <sjtommylee@gmail.com>
2023-05-15 20:34:22 -0700

    chore: commit save point

--- arrays-hashing/contains-duplicate/cd.py
+++ arrays-hashing/contains-duplicate/cd.py
@@ -6,12 +6,10 @@ b = [1, 2, 3, 1] # true
 
 # Try 1 Brute force
 """
-1. instantiate a class named "Solution"
-2. method named containsDuplicate, which takes in a list of integers. expect a boolean return
-3. first loop, we just go for the entire length of nums
-4. the second for loop, we start at indices i + 1 to length of nums
-5. we check to see if nums[i] == nums[j]. because if there are two identical values in different positions, there is a "duplicate"
-6. return True if there is a match, otherwise return False 
+1. first loop, we just go for the entire length of nums
+2. the second for loop, we start at indices i + 1 to length of nums
+3. we check to see if nums[i] == nums[j]. because if there are two identical values in different positions, there is a "duplicate"
+4. return True if there is a match, otherwise return False 
 
 Time: 
 o(n^2)
@@ -33,13 +31,11 @@ print(solution.containsDuplicate(a))
 
 # Try 2 Hashmap
 """
-1. instantiate a class named solution
-2. method named containsDuplicate, which takes in a list of integers. expect a boolean return
-3. create a hashset, where we are going to store unique elements we encounter
-4. for each element i in nums,
-5. if i already exists in the hashset, we return True. 
-6. we add each element i into the hashset
-7. if we encounter 0 duplicates, return false
+1. create a hashset, where we are going to store unique elements we encounter
+2. for each element i in nums,
+3. if i already exists in the hashset, we return True. 
+4. we add each element i into the hashset
+5. if we encounter 0 duplicates, return false
 
 time: 
 o(n)
@@ -59,3 +55,22 @@ class Solution2:
     
 hashmap = Solution2()
 print(hashmap.containsDuplicate(b))
+
+
+# Try 3 Sort
+"""
+1. we run sort() to nums, where for example if our list is [1, 2, 3, 1], it will change this to [1, 1, 2, 3]
+2. we compare adjascent elements with nums[i] and nums[i + 1] to see if we have a duplicate
+
+"""
+class Solution3:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+    
+sort = Solution3()
+print(sort.containsDuplicate(b))
