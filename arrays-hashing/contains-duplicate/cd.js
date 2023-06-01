116e951c44b8666ee066931e22b1fe1c20bdcc53
sjtommylee <sjtommylee@gmail.com>
2023-06-01 14:22:54 -0700

    chore: commit save point

---
+++ arrays-hashing/contains-duplicate/cd.js
@@ -0,0 +1,13 @@
+var a = [1, 2, 3, 4]; // false
+var b = [1, 2, 3, 1]; // true
+function containsDuplicate(nums) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+console.log(containsDuplicate(a));
---
+++ arrays-hashing/contains-duplicate/cd.ts
@@ -0,0 +1,15 @@
+const a = [1, 2, 3, 4]; // false
+const b = [1, 2, 3, 1]; // true
+
+function containsDuplicate(nums: number[]): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(containsDuplicate(a));
