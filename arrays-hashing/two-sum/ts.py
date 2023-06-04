040c5f5c8e3346c030276a1c7c311a65ec0a4787
sjtommylee <sjtommylee@gmail.com>
2023-06-03 19:00:40 -0700

    chore: commit save point

---
+++ arrays-hashing/two-sum/ts.py
@@ -0,0 +1,11 @@
+from typing import List
+
+nums = [3, 2, 4]
+target = 6
+
+# Try 1 - Brute force
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        return 
