a03264627b5696e115741e95526db48836467e0e
sjtommylee <sjtommylee@gmail.com>
2023-06-03 19:20:59 -0700

    hashmap

--- arrays-hashing/two-sum/ts.py
+++ arrays-hashing/two-sum/ts.py
@@ -31,3 +31,26 @@ class Solution:
 solution = Solution()
 result = solution.twoSum(nums, target)
 print(result)
+
+
+#Try 2 - Hash map
+"""
+1. we first start by initializing an empty dictionary 'previous'. we are going to store the previously encoutnered elements and returns a list of integers
+2. we can use enumerate to iterate through over the nums list, where for each element n the index i
+3. diff - the difference between the target and n
+4. The variable diff represents the difference between the target value and the current element n being iterated over in the nums list. By calculating diff, we determine the complement that we need to find in order to reach the target sum.
+5. if we find the diff already exists in the dictionary, we return the index of the complement and the current index
+
+
+"""
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        previous = {} # val: index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in previous:
+                return [previous[diff], i]
+            previous[n] = i
+        return
