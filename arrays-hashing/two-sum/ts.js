12174cd211acc0af602860be99b367b8a414c0b0
sjtommylee <sjtommylee@gmail.com>
2023-07-01 12:42:50 -0700

    chore: commit save point

--- arrays-hashing/two-sum/ts.js
+++ arrays-hashing/two-sum/ts.js
@@ -1,7 +1,17 @@
-var nums = [3, 2, 4];
-var target = 6;
+var nums = [3, 2, 4, 8, 10, 21];
+var target = 23;
+/**
+ * Try 1: brute force
+ */
 function twoSum(nums, target) {
-    console.log(nums, target);
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                console.log([i, j]);
+                return [i, j];
+            }
+        }
+    }
     return [];
 }
 twoSum(nums, target);
--- arrays-hashing/two-sum/ts.ts
+++ arrays-hashing/two-sum/ts.ts
@@ -1,12 +1,19 @@
-const nums = [3, 2, 4];
-const target = 6;
+const nums = [3, 2, 4, 8, 10, 21];
+const target = 23;
 
 /**
  * Try 1: brute force
  */
 
 function twoSum(nums, target): number[] {
-  console.log(nums, target);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log([i, j]);
+        return [i, j];
+      }
+    }
+  }
   return [];
 }
 
