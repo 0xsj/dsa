716ac12fc285bf8aaf34c960a7909a5fc85b7c3e
sjtommylee <sjtommylee@gmail.com>
2023-07-01 12:33:18 -0700

    chore: commit save point

---
+++ arrays-hashing/two-sum/ts.js
@@ -0,0 +1,7 @@
+var nums = [3, 2, 4];
+var target = 6;
+function twoSum(nums, target) {
+    console.log(nums, target);
+    return [];
+}
+twoSum(nums, target);
--- arrays-hashing/two-sum/ts.ts
+++ arrays-hashing/two-sum/ts.ts
@@ -1,7 +1,13 @@
 const nums = [3, 2, 4];
 const target = 6;
 
+/**
+ * Try 1: brute force
+ */
+
 function twoSum(nums, target): number[] {
   console.log(nums, target);
   return [];
 }
+
+twoSum(nums, target);
