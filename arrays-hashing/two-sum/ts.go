c7b3ef92cfd38312331a7cc7cc401798102108eb
sjtommylee <sjtommylee@gmail.com>
2023-07-03 15:45:53 -0700

    chore: commit save point

---
+++ arrays-hashing/two-sum/ts.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	fmt.Println(nums)
+	fmt.Println(target)
+
+	// return results
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	target := 6
+	twoSum(nums, target)
+}
