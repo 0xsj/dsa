b065762ee08b85589b6ece236b2982b5f66b83f4
sjtommylee <sjtommylee@gmail.com>
2023-07-04 15:25:41 -0700

    chore: commit save point

---
+++ arrays-hashing/group-anagrams/ga.ts
@@ -0,0 +1,10 @@
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+const results = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]];
+
+/**
+ * Try 1
+ */
+
+function groupAnagrams(strs: string[]): string[][] {
+  return [];
+}
---
+++ two-pointers/remove-duplicate/rd.ts
@@ -0,0 +1,11 @@
+const arr1 = [3, 2, 2, 3];
+const val1 = 2;
+const arr2 = [0, 1, 2, 2, 3, 0, 4, 2];
+const val2 = 2;
+
+function removeElement(nums: number[], val: number): number {
+  return;
+}
+
+removeElement(arr1, val1);
+removeElement(arr2, val2);
